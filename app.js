41100acedcbfd35866ad4f86a801132ca7c4ddda
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-11-04 08:08:52 +0000

    Testing concept

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const endpointPeopleInSpace = 'http://api.open-notify.org/astros.json';
-const endpointISSPosition = 'http://api.open-notify.org/iss-now.json';
-const endpointEarthMeteoriteLanding = 'https://data.nasa.gov/resource/y77d-th95.json';
+// const endpointPeopleInSpace = 'http://api.open-notify.org/astros.json';
+// const endpointEarthMeteoriteLanding = 'https://data.nasa.gov/resource/y77d-th95.json';
 
+const endpointISSPosition = 'http://api.open-notify.org/iss-now.json';
+const mapboxImage = document.querySelector('img.map');
 const ISSPosition = [];
 
-function fetchEndpoint() {
-  fetch(endpointISSPosition)
-  .then(blob => blob.json())
-  .then(data => ISSPosition.push(data));
+function fetchEndPoint() {
 
-  console.log(ISSPosition);
+  fetch(endpointISSPosition)
+    .then(blob => blob.json())
+    .then(data => ISSPosition.push(data));
+
+}
+
+function getMapCoordinates() {
+
+  let long = parseFloat(ISSPosition[0].iss_position.longitude);
+  let lat = parseFloat(ISSPosition[0].iss_position.latitude);
+  
+  const endpointMapbox = `https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${long},${lat},3,0/1280x600@2x?access_token=pk.eyJ1Ijoiam9lYXNod2VsbCIsImEiOiJja2gxdzBkN2MwOHdtMnVsc2t5MG1jOGJ2In0.LG1Bdwg4-nRKZ0gOic6BPw`;
+  
+  mapboxImage.src = endpointMapbox;
 
 }
 
 
-setTimeout(function getMapCoordinates() {
-  let long = parseFloat(ISSPosition[0].iss_position.longitude);
-  let lat = parseFloat(ISSPosition[0].iss_position.latitude);
-  
-  const endpointMapbox = `https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${long},${lat},5,0/1280x1280@2x?access_token=pk.eyJ1Ijoiam9lYXNod2VsbCIsImEiOiJja2gxdzBkN2MwOHdtMnVsc2t5MG1jOGJ2In0.LG1Bdwg4-nRKZ0gOic6BPw`;
-  
-  const mapboxImage = document.querySelector('img.map-iframe');
-  
-  mapboxImage.src = endpointMapbox;
-}, 1000)
-
-window.addEventListener('load', fetchEndpoint);
+setTimeout(getMapCoordinates, 1500);
+setInterval(fetchEndPoint, 1000);
