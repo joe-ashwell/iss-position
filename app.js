8dd5a6213f1857643306441d40a833a829ec4553
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-11-06 09:54:26 +0000

    Working prototype

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const endpointISSPosition = 'http://api.open-notify.org/iss-now.json';
 const mapboxImage = document.querySelector('img.map');
 const ISSPosition = [];
 
-function fetchEndPoint() {
+setInterval(function fetchEndPoint() {
 
   fetch(endpointISSPosition)
     .then(blob => blob.json())
@@ -15,12 +15,12 @@ function fetchEndPoint() {
     })
     .then(getMapCoordinates)
 
-};
+}, 1000);
 
 function getMapCoordinates() {
 
-  let long = parseFloat(ISSPosition[0].iss_position.longitude);
-  let lat = parseFloat(ISSPosition[0].iss_position.latitude);
+  let long = parseFloat(ISSPosition[ISSPosition.length - 1].iss_position.longitude);
+  let lat = parseFloat(ISSPosition[ISSPosition.length - 1].iss_position.latitude);
   
   const endpointMapbox = `https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${long},${lat},3,0/1280x600@2x?access_token=pk.eyJ1Ijoiam9lYXNod2VsbCIsImEiOiJja2gxdzBkN2MwOHdtMnVsc2t5MG1jOGJ2In0.LG1Bdwg4-nRKZ0gOic6BPw`;
   
@@ -28,5 +28,5 @@ function getMapCoordinates() {
 
 }
 
-// window.addEventListener('load', fetchEndPoint);
-setInterval(fetchEndPoint, 1000);
+// Firstly, got stuck due to bad syntax on my end (using 'getMapCoordinates()' in the promise)
+// Secondly, forgot I had defined the long and lat as ISSPosition[0], so even though it was updating the array, it wasn't updated the screen.
